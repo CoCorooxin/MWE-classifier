7e9000e74e7ecfa598865023f4358da84e7b138e
xin <chenxin9812@gmail.com>
2023-06-17 18:32:40 +0200

    update

---
+++ lstm_classifier.py
@@ -0,0 +1,241 @@
+from data_utils import Vocabulary
+from models import AttentionLSTM, LSTMmwe
+from torch.utils.data import Dataset, DataLoader, random_split
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from crf import CRF
+import os
+from lstm_dataset import LstmDataset
+from gensim.models import KeyedVectors, Word2Vec
+
+
+class MweLSTM(nn.Module):
+
+    def __init__(self, name, toks_vocab, tags_vocab, emb_size=64, hidden_size=64, drop_out=0., pretrained = False, device = "cpu"):
+
+        super(MweLSTM, self).__init__()
+
+        self.toks_vocab   = toks_vocab
+        self.tags_vocab   = tags_vocab
+        self.padidx       = tags_vocab["<pad>"]
+        self.emb_size     = emb_size
+
+        self.word_embedding = nn.Embedding(len(toks_vocab), emb_size).to(device)
+        if pretrained:
+            self._load_pretrained()
+
+        self.relu         = nn.ReLU()
+        self.dropout = nn.Dropout(drop_out)
+
+        if name == "LSTM":
+            self.rnn = LSTMmwe(emb_size, hidden_size, device = device)
+        if name == "ATLSTM":
+            self.rnn = AttentionLSTM(emb_size, hidden_size, drop_out = drop_out, device = device)
+        self.crf = CRF(hidden_size, self.tags_vocab)
+
+    def forward(self, Xtoks_IDs, masks):
+
+        emb = self.word_embedding(Xtoks_IDs)#+ self.depl_embedding(deprel)  # Batch, inputsize*emb_size
+        #print(emb.shape) #B, window_size, emb_size
+        # input.view(b, -1) B, window_size * emb_size
+        logits, _ = self.rnn(emb, masks)   #B, seq, hiddensize
+
+        #print(output.shape)
+        return logits # bs, seq, embsize
+
+    def _load_pretrained(self):
+        word_vectors = KeyedVectors.load_word2vec_format("corpus/frWac_non_lem_no_postag_no_phrase_200_cbow_cut100.bin", binary=True, limit=500000)
+        pretrained_weights = []
+        for idx,word in enumerate(self.toks_vocab.idx2word):
+            if word in word_vectors:
+                pretrained_weights.append(torch.tensor(word_vectors[word]))
+            else:
+                pretrained_weights.append(torch.FloatTensor(self.emb_size).uniform_(-0.25, 0.25))  # Randomly initialize for unknown words
+        pretrained_weights = torch.stack(pretrained_weights)
+        self.word_embedding.weight.data.copy_(pretrained_weights)
+        self.word_embedding.weight.requires_grad = True
+
+    def train_model(self, train_data, test_data, dev_data, epochs=10, lr=1e-3, batch_size=10, device="cpu", split =0.8):
+        """
+        the train data is in form of nested lists: [sentences[tokens]]
+        """
+        self.to(device)
+        # adaptive gradient descent, for every update lr is a function of the amount of change in the parameters
+        optimizer   = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=1e-5)
+        #optimizer = torch.optim.SGD(self.parameters(), lr=lr, momentum=0.9)
+        test_loader, dev_loader = test_data.get_loader(batch_size=batch_size * 10), dev_data.get_loader(batch_size=batch_size * 10)
+        train_loader = train_data.get_loader(batch_size=batch_size)
+        train_loss = []
+
+        for e in range(epochs):
+            self.train()
+            ep_loss = []
+
+            for X_toks, Y_gold in tqdm(train_loader):
+                bs, seq = X_toks.shape
+                optimizer.zero_grad()
+                masks  = (X_toks != self.toks_vocab["<pad>"]).to(device)  #set padding idx to false
+                logits = self.forward(X_toks.to(device), masks)
+                #print(X_toks.shape)
+                loss =  self.crf.loss(logits, Y_gold.to(device), masks) #conditional random field
+                #loss_value = loss_fnc(logprobs.view(bs*seq, -1), Y_gold.view(-1))
+                ep_loss.append(loss.mean().item())
+                loss.backward(loss)
+                optimizer.step()
+
+            loss = sum(ep_loss) / len(ep_loss)
+            train_loss.append(loss)
+            valid_loss = self.validate(dev_loader, device = device)
+
+            # print("Epoch %d | Mean train loss  %.4f | Mean dev loss %.4f"%(e,loss, devloss) )
+            print("Epoch %d | Mean train loss  %.4f |  Mean dev loss  %.4f " % (e, loss, valid_loss))
+            print()
+
+        class_counts, TP, FP, FN, average_precision, average_recall, average_f1_score, weighted_f1_score, weighted_recall, weighted_precision = self.evaluate(test_loader)
+        print("AVR: Precision %.4f | Recall  %.4f |  F-score  %.4f " % (average_precision, average_recall, average_f1_score))
+        print("Weighted: Precision %.4f | Recall  %.4f |  F-score  %.4f " % (weighted_f1_score, weighted_recall, weighted_precision))
+
+    def validate(self, data_loader, device="cpu"):
+        self.to(device)
+        loss_lst = []
+        self.eval()
+        with torch.no_grad():
+            for X_toks, Y_gold in tqdm(data_loader):
+                bs, seq = X_toks.shape
+                masks = (X_toks != self.toks_vocab["<pad>"]).to(device) #create a mask where padidx is false
+                logits = self.forward(X_toks.to(device), masks)
+                #loss = loss_fnc(logprobs.view(bs*seq, -1), Y_gold.view(-1))
+                loss = self.crf.loss(logits , Y_gold.to(device), masks)
+
+                loss_lst.append(loss)
+        return sum(loss_lst) / len(loss_lst)
+
+    def predict(self, batch_tensors, device = "cpu"):
+        """
+        :param batch_tensors: a batch tensor of input seqs
+        :return: the predicted best path and the scores
+        """
+        self.eval()
+        self.to(device)
+        with torch.no_grad():
+
+            masks = (batch_tensors != self.toks_vocab["<pad>"]).to(device)
+            logits          = self.forward(batch_tensors, masks)
+            best_score, best_paths = self.crf(logits, masks)
+
+        return best_score, best_paths
+
+    def evaluate(self, test_loader, device = "cpu"):
+        """
+        evaluation the classifier with confusion matrix : precision recall and f-score
+        """
+        self.to(device)
+        self.eval()
+        num_tags = len(self.tags_vocab)
+        # print(num_tags)
+        TP = torch.zeros(num_tags)
+        FP = torch.zeros(num_tags)
+        FN = torch.zeros(num_tags)
+        class_counts = torch.zeros(num_tags)
+        with torch.no_grad():
+            for X_toks, Y_golds in tqdm(test_loader):
+
+                masks  = (X_toks != self.toks_vocab["<pad>"]).to(device)
+                # Forward pass
+                logits = self.forward(X_toks.to(device), masks)
+                best_score, best_paths = self.crf(logits, masks) #viterbi
+                best_paths = best_paths
+                #print(best_paths.shape)
+                for i in range(len(best_paths)):
+                    path = torch.tensor(best_paths[i])
+                    #print(Y_golds[i])
+                    gold = torch.tensor([j for j in Y_golds[i] if j != self.padidx])
+                    #print(path)
+                    #print(gold)
+                    for tag in range(num_tags):
+                        TP[tag] += ((path == tag) & (gold == tag)).sum()
+                        FP[tag] += ((path == tag) & (gold != tag)).sum()
+                        FN[tag] += ((path != tag) & (gold == tag)).sum()
+                        class_counts[tag] += (gold==tag).sum()
+
+        # Calculate precision, recall, and F1 score for each tag
+        precision = TP / (TP + FP)
+        # avoid nan
+        nan_mask = torch.isnan(precision)
+        precision[nan_mask] = 0.
+
+        recall = TP / (TP + FN)
+        # avoid nan
+        nan_mask = torch.isnan(recall)
+        recall[nan_mask] = 0.
+
+        f1_score = 2 * (precision * recall) / (precision + recall)
+        # avoid nan
+        nan_mask = torch.isnan(f1_score)
+        f1_score[nan_mask] = 0.
+        # Calculate class weights
+        class_weights = class_counts / class_counts.sum()
+
+        # Calculate average precision, recall, and F1 score
+        average_precision = torch.mean(precision)
+        average_recall = torch.mean(recall)
+        average_f1_score = torch.mean(f1_score)
+
+        weighted_f1_score = torch.sum(f1_score * class_weights)
+        weighted_recall = torch.sum(recall * class_weights)
+        weighted_precision = torch.sum(precision * class_weights)
+
+        return class_counts, TP, FP, FN, average_precision, average_recall, average_f1_score, weighted_f1_score, weighted_recall, weighted_precision
+
+    @staticmethod
+    def load(modelfile,name, toks_vocab, tags_vocab, embsize, hidden_size, drop_out,  pretrained = False, device="cpu"):
+        toks_vocab = Vocabulary.read(toks_vocab)
+        tags_vocab = Vocabulary.read(tags_vocab)
+        model      = MweLSTM(name, toks_vocab,tags_vocab, embsize, hidden_size, drop_out, pretrained, device)
+        model.load_params(modelfile,device)
+        return model,toks_vocab, tags_vocab
+
+    def load_params(self,param_filename,device):
+        self.load_state_dict(torch.load(param_filename, map_location=device))
+
+    def save(self, path, name):
+        self.toks_vocab.write(os.path.join(path, "toks.txt"))
+        self.tags_vocab.write(os.path.join(path, "tags.txt"))
+        torch.save(self.state_dict(), os.path.join(path, name))
+
+
+if __name__ == '__main__':
+    import argparse
+    import os
+    import yaml
+    parser = argparse.ArgumentParser("MWE Classifer")
+    parser.add_argument('config_file')
+
+    args = parser.parse_args()
+    cstream = open(args.config_file)
+    config = yaml.safe_load(cstream)
+    lr = float(config["LR"])
+
+    embsize = int(config["EMBSIZE"])
+    hidsize = int(config["HIDDENSIZE"])
+    bs      = int(config["BATCHSIZE"])
+    epochs  = int(config["EPOCHS"])
+    dropout = float(config["DROPOUT"])
+    train   = LstmDataset(config["TRAIN"], isTrain = True)
+    test    = LstmDataset(config["TEST"])
+    dev     = LstmDataset(config["DEV"])
+    device  = config["DEVICE"]
+    pretrain= config["PRETRAINED"]
+
+    cstream.close()
+    toks_vocab  = train.toks_vocab
+    tags_vocab  = train.tags_vocab
+    model       = MweLSTM(config['NAME'], toks_vocab, tags_vocab, embsize,hidsize, dropout, pretrained = pretrain, device = device)
+    #train_data, test_data, epochs=10, lr
+    #(self, train_data, test_data, epochs=10, lr=1e-3, batch_size=10, device="cpu", split=0.8):
+    model.train_model(train, test, dev, epochs, lr, bs, device = device)
+
+
+    model.save(config["MODEL_DIR"], config["MODEL_FILE"])
+
---
+++ lstm_dataset.py
@@ -0,0 +1,132 @@
+from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.nn.functional import pad
+from collections import Counter
+import itertools
+from data_utils import Vocabulary
+
+"""
+Functions for reading and writing UD CONLL data
+"""
+CONLL_FIELDS = ["token", "pos", "features", "deprel"]
+MWE_TAGS = ["B", "I"]  # B for begin , I for inside
+
+def readfile(filename, update=False, toks_vocab=Vocabulary([ "<unk>","<pad>"]), tags_vocab=Vocabulary([ "<unk>", "<pad>"])):
+    """
+    function to read the corpus at one pass
+    signature for train corpus : X_toks, Y_tags = readfile("corpus/train.conllu", update=True)
+    signature for test corpus/ dev corpus:  X_test, Y_test = readfile("corpus/train.conllu", update=True, vocabtoks_train, vocabtags_train)
+    """
+
+    istream = open(filename, encoding="utf-8")
+    corpus = []
+    toks, mwes = [], []
+    for line in istream:
+        line = line.strip()
+        if line and line[0] != "#":
+            try:
+                tokidx, token, lemma, pos, upos, features, headidx, deprel, extended, _ = line.split()
+
+            except ValueError:
+                pass
+
+
+            # extract simple mwe tags
+            #mwe_tag = lambda x: "I" if features.startswith("component") else "B"
+            #tag = mwe_tag(features) + "_" + upos2pos(pos)
+            mwe = ''
+            if features.startswith("component"):
+                mwe = "I" + "_"+(pos)
+            elif features.startswith("mwe"):
+                mwe = "B" + "_"+(pos)
+            else:
+                mwe = "O" + "_"+(pos)
+
+            toks.append(token)
+            mwes.append(mwe)
+
+            if update:
+                toks_vocab.update(token)
+                tags_vocab.update(mwe)
+
+        elif toks:
+            # end of sentence, add  false tokens
+            #corpus.append({"tok": toks+ ["<eos>"], "mwe":mwes+["<eos>"], "deprel": deprels+ ["<eos>"] })
+            corpus.append({"tok": toks , "mwe": mwes })
+            toks, mwes = [], []
+
+    istream.close()
+    # return the encoded data in list of list, the nested list represents the sentences
+    return corpus, toks_vocab, tags_vocab
+
+
+# [{"token1": "token", "multiword": "mwe", "mwe lemma": "mwe lemma"}, {"token2": "token", "multiword": "mwe"}, {"token3": "token", "multiword": "mwe"}]
+
+class LstmDataset(Dataset):
+
+    def __init__(self, datafilename=None, toks_vocab=Vocabulary([ "<unk>", "<pad>"]), tags_vocab=Vocabulary([ "<unk>","<pad>"]), isTrain=False):
+        """
+        take as input either the path to a conllu file or a list of tokens
+        we consider context size as the n preceding and n subsequent words in the text as the context for predicting the next word.
+        """
+        super(LstmDataset, self).__init__()
+
+        self.toks_vocab, self.tags_vocab  = toks_vocab, tags_vocab
+        if datafilename:
+            self.corpus, self.toks_vocab, self.tags_vocab = readfile(datafilename, update=isTrain, toks_vocab=toks_vocab, tags_vocab=tags_vocab)
+            self.data = self.corpus
+
+        print('token Vocab size', len(self.toks_vocab))
+
+    def __len__(self):
+        return len(self.data)
+
+    @staticmethod
+    def from_string(sentence, toks_vocab, tags_vocab):
+        toks = sentence.split()
+        len_sent = len(toks)
+        dataset      = LstmDataset(toks_vocab, tags_vocab)
+        dataset.data = [{"tok": [toks], "mwe": ["<unk>" for i in range(len_sent)]}]
+
+    @property
+    def tag_padidx(self):
+        return self.tags_vocab.lookup("<pad>")
+
+    def __getitem__(self, idx):
+
+        x = self.data[idx]["tok"]
+        mwe = self.data[idx]["mwe"]
+
+        return x, mwe
+
+    def as_strings(self, batch_tensor):
+        """
+        Returns a string representation of a tensor of word indexes
+        """
+        out = []
+        for line in batch_tensor.tolist():
+            out.append([self.toks_vocab.rev_lookup(idx) for idx in line])
+        return out
+
+    def get_loader(self, batch_size=1, num_workers=0, shuffle=False):
+        def mk_batch(selected_items):
+            XtoksIDs = []
+            YtagsIDs = []
+            max_seq_len = max(len(sent) for sent,_ in selected_items)+1
+
+            for sent, mwe in selected_items:
+
+                x = torch.tensor([self.toks_vocab[tok] for tok in sent])
+                y = torch.tensor([self.tags_vocab[tag] for tag in mwe])
+
+                XtoksIDs.append(pad(x, (0, max_seq_len-len(x)), value = self.toks_vocab["<pad>"]))
+                YtagsIDs.append(pad(y, (0, max_seq_len-len(y)), value = self.tags_vocab["<pad>"]))
+            return torch.stack(XtoksIDs), torch.stack(YtagsIDs)
+
+        # specify that the mk_batch function should be used to collate the individual samples into batches.
+        return DataLoader(self, batch_size=batch_size, num_workers=num_workers, shuffle=shuffle, collate_fn=mk_batch)
+
+
+
+if __name__ == '__main__':
+    corpus, toks_vocab, tags_vocab, deprel_vocab = readfile("corpus/train.conllu")
